f02e027d0b0acf9b651ff44539f427469675987f
shunlog <artiombalan331@gmail.com>
2023-03-07 10:19:37 +0200

    generalize grammar & NFA data structures

--- code/main.py
+++ code/main.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
+from icecream import ic
 
 class Grammar:
+    '''
+    A grammar is represented by 4 variables:
+    VN - list of nonterminals (strings)
+    VT - list of terminals (strings)
+    P - list of productions represented by a dictionary, where
+        keys are rules and values are lists of rules.
+        A rule is a tuple of terminals and nonterminals
+
+    Example:
+
+    The formal grammar
+
+    A -> aA
+    A -> bB
+    A -> B
+    B -> ε
+
+    Is represented by the following variables
+
+    VN = {"A", "B"}
+    VT = {"a", "b"}
+    P = {
+        ("A"): [("a", "A"), ("b", "B"), ("B")],
+        ("B"): [()]
+    }
+    S = "A"
+    '''
     def __init__(self, VN, VT, P, S):
         self.VN = VN
         self.VT = VT
@@ -8,29 +36,41 @@ class Grammar:
         self.S = S
 
     def constr_word(self):
+        '''Assuming right-regular grammar,
+        build a word by randomly picking rules to rewrite'''
         from random import choice
-        w = self.S
-        while w[-1] in self.VN:
-            # There's a slight probability this will go on for too long
-            N = w[-1]
-            w = w[:-1] + choice(self.P[N])
-        return w
+        s = self.S  # current state
+        w = ""  # word
+        while s in self.VN:
+            rule = choice(self.P[(s)])
+            w += rule[0]
+            s = rule[1]
+        return ''.join(w)
 
-    def to_FSM(self):
-        fin_state = "End"
-
-        d = {}
-        for s0, rl in self.P.items():
-            d |= {(s0, r[0]): r[1] if len(r) == 2 else fin_state for r in rl}
-
-        return FSM(self.VN | {fin_state}, self.S, d, {fin_state})
 
 class FSM:
+    '''
+    A Finite State Machine is represented by 4 variables:
+    S - set of states (strings)
+    s0 - initial state (string)
+    d - the state-transition function (dictionary: (state, rule) -> list of rules)
+    F - set of final states (must be subset of S)
+    '''
     def __init__(self, S, s0, d, F):
         self.S = S  # states
         self.s0 = s0  # initial state
         self.d = d  # transitions
-        self.F = F # finite states (subset of S)
+        self.F = F # final states (subset of S)
+
+    def from_grammar(g : Grammar):
+        d = {}
+        for s0, rl in g.P.items():
+            d |= {(s0, r[0]): r[1] for r in rl}
+
+        return FSM(S = g.VN | {"ε"},
+                   s0 = g.S,
+                   d = d,
+                   F = {"ε"})
 
     def verify(self, w):
         s = self.s0
@@ -40,6 +80,9 @@ class FSM:
                 return False
             s = ns
         return s in self.F
+
+    def __repr__(self):
+        return ', '.join([str(x) for x in [self.S, self.s0, self.d, self.F]])
 
 if __name__ == '__main__':
     # Variant #3 is non-deterministic (didn't reserve enough time for that)
@@ -53,15 +96,23 @@ if __name__ == '__main__':
     # Variant #2
     VN = {"S", "R", "L"}
     VT = {"a", "b", "c", "d", "e", "f"}
-    P = {"S": ["aS", "bS", "cR", "dL"],
-         "R": ["dL", "e"],
-         "L": ["fL", "eL", "d"]}
+    P = {("S"): [( "a" , "S" ), ( "b", "S" ), ( "c", "R" ), ( "d", "L" )],
+         ("R"): [( "d", "L" ), ( "e", "ε" )],
+         ("L"): [( "f", "L" ), ( "e", "L" ), ( "d", "ε" )]}
     S = "S"
 
     g = Grammar(VN, VT, P, S)
-    fsm = g.to_FSM()
-
-    for i in range(10):
+    m, ml = "", 0
+    for _ in range(1000):
         w = g.constr_word()
-        assert(fsm.verify(w))
-        assert(not fsm.verify(w+"a"))
+        if len(w) > ml:
+            ml = len(w)
+            m = w
+    ic(m)
+    fsm = FSM.from_grammar(g)
+    ic(fsm)
+
+    # for i in range(10):
+    #     w = g.constr_word()
+    #     assert(fsm.verify(w))
+    #     assert(not fsm.verify(w+"a"))
