e15592215bf250ac96e020d7aa2461e7ec10cb58
shunlog <artiombalan331@gmail.com>
2023-05-23 00:26:26 +0300

    passing simple expressions, Parser class

--- src/python_subset/parser.py
+++ src/python_subset/parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
 from typing import Union
+from lexer import *
 from icecream import ic
 
 @dataclass
@@ -12,19 +13,71 @@ class Number:
     value: str
 
 @dataclass
-class OpFactor:
+class Factor:
     op: Union[str, None]
-    factor: Union[Variable, Number, 'Expression']
+    value: Union[Variable, Number, 'Expression']
 
 @dataclass
-class OpTerm:
+class Term:
     op: Union[str, None]
-    term: list[OpFactor]
+    factors: list[Factor]
 
 @dataclass
 class Expression:
-    opterms: list[OpTerm]
+    terms: list[Term]
 
 
-def expression(tokens):
-    return None
+class Parser:
+    def accept(self, toktype, values=()):
+        # if first token is toktype, consume it and return it
+        # otherwise don't consume it and return False
+        if self.tokens and self.tokens[0].type == toktype \
+            and (not values or self.tokens[0].value in values):
+            self.tok = self.tokens.pop(0)
+            return True
+        return False
+
+
+    def expect(self, toktype, values=()):
+        # if next token is not toktype, raise error,
+        # otherwise return the token
+        if not self.accept(toktype, values):
+            raise ValueError
+        return
+
+
+
+    def factor(self):
+        if self.accept(TokenType.ID):
+            return Variable(self.tok.value)
+        elif self.accept(TokenType.NUMBER):
+            return Number(self.tok.value)
+
+
+    def term(self):
+        factors = []
+        n = self.factor()
+        factors.append(Factor(None, n))
+        while self.accept(TokenType.OPERATOR, ('*', '/')):
+            op = self.tok.value
+            n = self.factor()
+            factors.append(Factor(op, n))
+
+        return factors
+
+
+    def expression(self):
+        terms = []
+        n = self.term()
+        terms.append(Term(None, n))
+        while self.accept(TokenType.OPERATOR, ('+', '-')):
+            op = self.tok.value
+            n = self.term()
+            terms.append(Term(op, n))
+
+        return Expression(terms)
+
+
+    def parse(self, tokens):
+        self.tokens = tokens
+        return self.expression()
--- src/python_subset/test_parser.py
+++ src/python_subset/test_parser.py
@@ -5,33 +5,39 @@ from parser import *
 from lexer import *
 
 @pytest.mark.parametrize("tokens, AST",[
+    # '1'
+    (
+        [Token(TokenType.NUMBER, '1')],
+        Expression([Term(None, [Factor(None, Number('1'))])])
+    ),
 
     # 'a + 1'
-(
-    [
-        Token(TokenType.ID, 'a'),
-        Token(TokenType.OPERATOR, '+'),
-        Token(TokenType.NUMBER, '1')
-    ],
+    (
+        [
+            Token(TokenType.ID, 'a'),
+            Token(TokenType.OPERATOR, '+'),
+            Token(TokenType.NUMBER, '1')
+        ],
 
-    Expression(
-        [OpTerm(
-            None,
-            [OpFactor(
+        Expression(
+            [Term(
                 None,
-                Variable('a')
-            )]
-        ),
-         OpTerm(
-             '+',
-             [OpFactor(
-                 None,
-                 Number('1')
+                [Factor(
+                    None,
+                    Variable('a')
+                )]
+            ),
+             Term(
+                 '+',
+                 [Factor(
+                     None,
+                     Number('1')
+                 )]
              )]
-         )]
+        )
     )
-)
 
 ])
 def test_expr(tokens, AST):
-    assert expression(tokens) == AST
+    p = Parser()
+    assert p.parse(tokens) == AST
