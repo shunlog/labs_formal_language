f37d55bff44c989bda1228c8586d92488dfcbcd1
shunlog <artiombalan331@gmail.com>
2023-05-23 23:58:14 +0300

    main file to play with parser, separate inputs

---
+++ src/python_subset/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from icecream import ic
+from lexer import *
+from parser import *
+
+if __name__ == "__main__":
+    inp = sys.stdin.read()
+    tokens = get_tokens(inp)
+    ic(tokens)
+    p = Parser()
+    ast = p.parse(tokens)
+    ic(ast)
---
+++ src/python_subset/test_inputs/all.py
@@ -0,0 +1,18 @@
+# comments and blank lines are ignored
+
+# can have single expressions
+5 + 4
+
+# with parantheses too
+10 * (5 + 5 * 2)
+
+# assignments
+a = 10
+b = 0
+
+# while loops and conditionals
+while b == 0:
+    if a > b:
+        a = a - b
+    else:
+        b = b - a
